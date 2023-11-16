62d57c95be7adfe08a31f795a8bf6ca4a110223f
omarfrikha96 <omar.frikha96@gmail.com>
2023-11-16 23:37:22 +0100

    add daikichi path variable

---
+++ Week3/Core/DaikichiPathVariables/src/main/java/com/omar/Daikich/controller/Daikichi.java
@@ -0,0 +1,28 @@
+package com.omar.Daikich.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+@RequestMapping("/daikich")
+public class Daikichi {
+	@RequestMapping("")
+	public String display() {
+		return "Welcome!";
+	}
+	@RequestMapping("/travel/{city}")
+	public String travel(@PathVariable("city") String city) {
+		return "Congratulations! You will soon travel to " + city;
+	}
+	@RequestMapping("/lotto/{number}")
+	public String lotto(@PathVariable("number") Integer number) {
+		if(number % 2 == 0) {
+			return "You will take a grand journey in the near future, but be weary of tempting offers";
+		}
+		return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
+	}
+	
+}
