63aac8cdb9e751c9e5713f7b9928b7631f89e48b
omarfrikha96 <omar.frikha96@gmail.com>
2023-11-18 23:07:51 +0100

    add Hopper's Receipt assignment

---
+++ Week3/Practice/HopperReceipt/src/main/java/com/HopperReceipt/contoller/Controller.java
@@ -0,0 +1,26 @@
+package com.HopperReceipt.contoller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+    @RequestMapping("/")
+    public String index(Model model) {
+        
+        String name = "Grace Hopper";
+        String itemName = "Copper wire";
+        double price = 5.25;
+        String description = "Metal strips, also an illustration of nanoseconds.";
+        String vendor = "Little Things Corner Store";
+    
+    	// Your code here! Add values to the view model to be rendered
+        model.addAttribute("name", name);
+        model.addAttribute("itemName", itemName);
+        model.addAttribute("price", price);
+        model.addAttribute("description", description);
+        model.addAttribute("vendor", vendor);
+    
+        return "index.jsp";
+}
+    }
